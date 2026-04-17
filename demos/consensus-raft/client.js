39c3a6e94eee8433e46cc725c39bb34ed150e3a2
fbeilstein <yu.binkukoku@gmail.com>
2026-04-17 22:40:59 +0000

    raft client checks if req performed

--- demos/consensus-raft/client.js
+++ demos/consensus-raft/client.js
@@ -7,7 +7,7 @@ function onTimer(t) {
     if (t % 30 === 0 && cur_msg_id < COMMANDS.length && t > 25) {
         const s = loadState();
         const target = s.leaderId !== undefined ? s.leaderId : 0;
-        const cmd = COMMANDS[cur_msg_id++];
+        const cmd = COMMANDS[cur_msg_id];
         sendMessage(target, { type: 'ClientRequest', cmd }, 'blue');
     }
 }
@@ -17,6 +17,14 @@ function onMessage(m) {
     if (m.payload.leaderId !== undefined) {
         const s = loadState();
         s.leaderId = m.payload.leaderId;
+
+        if (m.payload.leaderLog && cur_msg_id < COMMANDS.length) {
+            const committedLogs = m.payload.leaderLog.slice(0, m.payload.commitIndex + 1);
+            if (committedLogs.some(l => l.cmd === COMMANDS[cur_msg_id])) {
+                cur_msg_id++;
+            }
+        }
+
         dumpState(s);
     }
 }
